b8b80e9ad74cde060fc8adcde09511d059c7a620
Keoma Quiroga <keomaquiroga@gmail.com>
2025-12-14 01:25:36 +0000

    Plot memoria Se ploteo el uso principal de la memoria principal y la memoria swap en tiempo real. Falta identificar para cada proceso el uso de memoria, de ser posible también el uso de CPU y almacenamiento.

--- ram.py
+++ ram.py
@@ -3,25 +3,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 import psutil
 
-def mostrar_cpu():
+def mostrar_memoria():
     fig = plt.figure()      # figura principal
     ax = fig.add_subplot(111)
     x = []      # guardar la cpu
     n = psutil.cpu_count()      # numero de cpu
 
     def update(frame, x):
-        x.append(psutil.cpu_percent(interval=1, percpu=True))
-        if len(x) > 60:     
+        mem = psutil.virtual_memory()
+        mem = mem.percent
+
+        swap = psutil.swap_memory()
+        swap = swap.percent
+
+        x.append([mem, swap])
+
+        if len(x) > 60:     # tras 60 segundos
             x.pop(0)        # eliminamos el primer elemento
         x = np.array(x)
 
         ax.clear()      # limpiamos la tabla
-        for i in range(n):
-            ax.plot(x[:, i], label=f"CPU {i+1}")        # ploteamos cada cpu
+        ax.plot(x[:,0], label="Memoria")
+        ax.plot(x[:,1], label="Memoria Swap")
         
-        ax.set_title("Uso CPU")
+        ax.set_title("Uso Memoria")
         ax.set_xlabel("Tiempo (s)")
-        ax.set_ylabel("% CPU")
+        ax.set_ylabel("% Uso")
         ax.legend()
         ax.set_xlim(0, 60)      # mostrar maximo un minuto
         ax.set_ylim(0, 100)     # 0% a 100%
@@ -29,4 +36,4 @@ def mostrar_cpu():
     ani = FuncAnimation(fig=fig, func=update, frames=60, fargs=(x,))
     plt.show()
 
-mostrar_cpu()
+mostrar_memoria()
